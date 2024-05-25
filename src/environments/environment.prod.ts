196f74af216a508f08f85165e4b31eb0794aa89f
Harsh00700 <harshgandhi90095@gmail.com>
2024-05-25 18:56:58 +0530

    Latest Production Environments with Firebase Hosting.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   // apiToken: 'a4df8ee82ff14c5f63ded9786f40ce832115fc282d6d1305b841e158afa7173dcad0dbd77253c5a3b105b7ef1baac41caae09a616c125e69eabc57766e02d6244ae19a26325f1936870aefe9e18bb24d8abc619c79dbf09ccb7c8befda262fa5c55077f3d6dc8160ad6c9b47b63b53ab01ccaca49e52a7251e9b6f756cb69806',
-  imageUrl: 'https://media.engginno.com/images',
+  imageUrl: 'https://media.engginno.com/images/',
   apiUrl: 'https://admin.engginno.com/api/',
 };
